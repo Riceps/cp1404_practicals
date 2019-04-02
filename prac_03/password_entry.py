b8ec7736f93a01e80959dd38cc4c36c8bd0d58f4
Jake <jake.rice@my.jcu.edu.au>
2019-04-02 12:27:40 +1000

    Changed password checker to utilise functions

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -1,15 +1,34 @@
-"""Jake"""
-MINIMUM_LENGTH = 4  # minimum password length
-valid_password = False # set a boolean password validator for while loop
+def main():
+    """Password Checker"""
+    # Ask user for password input
+    password = get_password()
+    # Print password length as '*'
+    print_password(password)
 
-# Ask user for password input
-password = str(input("Enter your password: "))
-while not valid_password:
-    # if password does not meet length requirements, request another password
-    if len(password) < MINIMUM_LENGTH:
-        print("Password must contain at least 4 characters")
-        password = str(input("Enter your password: "))
-    else:
-        valid_password = True
-# Print password length as '*'
-print("*" * len(password))
+
+def get_password():
+    """Retrieve password from user"""
+    valid_password = False  # set a boolean password validator for while loop
+    MINIMUM_LENGTH = 4  # minimum password length
+
+    password = str(input("Enter your password: "))
+    while not valid_password:
+        # if password does not meet length requirements, request another password
+        if len(password) < MINIMUM_LENGTH:
+            print("Password must contain at least 4 characters")
+            password = str(input("Enter your password: "))
+        # If password meets length requirements exit while loop
+        else:
+            valid_password = True
+    # Return the approved password to main
+    return password
+
+
+def print_password(password):
+    """Print password as asterisk"""
+    # Multiply asterisk by the length of password
+    print("*" * len(password))
+    return
+
+
+main()
