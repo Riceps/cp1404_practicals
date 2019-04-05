27f4ce4ba6649a3cc27a5b1abd13d41df6a68801
Riceps <jake.rice@my.jcu.edu.au>
2019-04-05 10:52:21 +1000

    Alter files with Lindsay

--- prac_03/broken_score_refactored.py
+++ prac_03/broken_score_refactored.py
@@ -1,3 +1,8 @@
+# Set an upper and lower limit
+UPPER_BOUNDARY = 100
+LOWER_BOUNDARY = 0
+
+
 def main():
     """Convert Score to grade"""
     # Receive a score between 0 and 100 inclusive from the user
@@ -8,13 +13,10 @@ def main():
 
 def score_to_grade(score):
     """Error check and return grade as string"""
-    # Set an upper and lower limit
-    UPPER_BOUNDARY = 100
-    LOWER_BOUNDARY = 0
     # Check which grade was received
     if UPPER_BOUNDARY < score or score < LOWER_BOUNDARY:
         return "invalid score"
-    elif 100 >= score >= 90:
+    elif score >= 90:
         return "Excellent"
     elif score >= 50:
         return "Pass"
--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -1,3 +1,6 @@
+MINIMUM_LENGTH = 4  # minimum password length
+
+
 def main():
     """Password Checker"""
     # Ask user for password input
@@ -9,17 +12,12 @@ def main():
 def get_password():
     """Retrieve password from user"""
     valid_password = False  # set a boolean password validator for while loop
-    MINIMUM_LENGTH = 4  # minimum password length
 
-    password = str(input("Enter your password: "))
-    while not valid_password:
+    password = input("Enter your password: ")
+    while len(password) < MINIMUM_LENGTH:
         # if password does not meet length requirements, request another password
-        if len(password) < MINIMUM_LENGTH:
             print("Password must contain at least 4 characters")
-            password = str(input("Enter your password: "))
-        # If password meets length requirements exit while loop
-        else:
-            valid_password = True
+            password = input("Enter your password: ")
     # Return the approved password to main
     return password
 
@@ -28,7 +26,6 @@ def print_password(password):
     """Print password as asterisk"""
     # Multiply asterisk by the length of password
     print("*" * len(password))
-    return
 
 
 main()
