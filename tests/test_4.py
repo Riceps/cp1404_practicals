b13e0b02425eb49fc0fe1cf0da786f56c57d7138
Riceps <jake.rice@my.jcu.edu.au>
2019-04-26 15:59:00 +1000

    complete test 4

---
+++ tests/test_4.py
@@ -0,0 +1,11 @@
+values = [1, 2, 3, 2]
+values.remove(2)
+print(values)
+
+before = [1, 4, 0, -1]
+after = before.sort()
+print(after)
+
+words = ["aye", "bee", "sea", "bee"]
+words.remove("bee")
+print(words.pop())
