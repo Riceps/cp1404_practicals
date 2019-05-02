7616fe51f30bea42f552e5961992b13d2bac5f02
Riceps <jake.rice@my.jcu.edu.au>
2019-05-03 01:04:18 +1000

    Create class for programming languages and client code for created class.

---
+++ prac_06/languages.py
@@ -0,0 +1,20 @@
+"""Client code to use the programming language class"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    # Print the given programming languages with class str method
+    print(ruby)
+    print(python)
+    print(visual_basic)
+
+    # Create list containing programming languages
+    programming_languages = [ruby, python, visual_basic]
+
+
+main()
---
+++ prac_06/programming_language.py
@@ -0,0 +1,16 @@
+class ProgrammingLanguage:
+    def __init__(self, program_name="", program_typing="", program_reflection=False, program_year=0):
+        self.program_name = program_name
+        self.program_typing = program_typing
+        self.program_reflection = program_reflection
+        self.program_year = program_year
+
+    def __str__(self):
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.program_name, self.program_typing,
+                                                                           self.program_reflection, self.program_year)
+
+    def is_dynamic(self):
+        if self.program_typing == "Dynamic":
+            return True
+        else:
+            return False
