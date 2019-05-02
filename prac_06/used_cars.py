6246cb5ffdf478ac4189ee35771e5cd2517e3310
Riceps <jake.rice@my.jcu.edu.au>
2019-05-03 00:28:24 +1000

    Complete Walk-through Example, modify existing Car class.

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,7 +6,14 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    limo = Car(100, "Limo")  # Add a car object with 100 units of fuel
+    limo.add_fuel(20)  # Add 20 more units of fuel to the new car
+    print("Limo fuel = {}".format(limo.fuel))  # Print the amount of fuel in the limo
+    limo.drive(115)  # Drive limo 115 km
+    print("Limo odo = {}".format(limo.odometer))  # Print the odometer of the limo
+    print(limo)
+
+    my_car = Car(180, "Beast Wagon")
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
